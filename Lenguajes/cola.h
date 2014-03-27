58721a933a29ad198e959001fcb46cb2b38276b6
melsandoval <m.san43@gmail.com>
2014-03-27 14:41:17 -0600

    Subiendo Pilas y colas

---
+++ Lenguajes/cola.h
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct _nodo {
+   int valor;
+   struct _nodo *siguiente;
+} tipoNodo;
+
+typedef tipoNodo *pNodo;
+
+/* Funciones con colas: */
+void Anadir(pNodo *primero, pNodo *ultimo, int v);
+int Leer(pNodo *primero, pNodo *ultimo);
